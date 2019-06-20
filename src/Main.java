05110cc03618982fa58dba3f09fcc0a2f9e00051
Roma021 <dtmogh@gmail.com>
2019-06-20 15:46:53 -0400

    ArrayPractice2

---
+++ src/Main.java
@@ -0,0 +1,82 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class Main {
+
+    public static void main(String [] args){
+        Scanner reader =new Scanner(System.in);
+        int typ;
+        // 1. Write a program to print a string in reverse
+
+       // String pr1 = "emoclew";
+
+      //  for(int x=0; x<=pr1.length();x++ ){
+         //   System.out.println(pr1);
+          //  if (x==0){
+           //     break;
+        //    }
+       // }
+     // 2. Write a program that will find the length of an input string without using the library function.
+      //  System.out.println("Type a Word: ");
+
+      //  String pr2 = reader.nextLine();
+
+     //   char [] jmp2 = pr2.toCharArray();
+     //   int x=0;
+     //   for( int i: jmp2){
+       //     x=+1;
+         //   System.out.println(i);
+     //   }
+     //   System.out.println(x);
+    // 3. Prompt the user to input 10 values and store them into an array. If there are any duplicate values in that array, remove them and print out the remaining values.
+
+      //  Integer myArray[] = new Integer[10];
+      //  for (int i = 0; i<myArray.length; i++){
+
+          //  System.out.println("Type in 10 values: " + (i+1));
+          //  typ = reader.nextInt();
+
+          //  myArray[i] = typ;
+           // for (int x=0; x<i; x++){
+           //     if (myArray[i]==myArray[x]){
+              //      myArray[i] = null;
+              //  }
+        //    }
+
+       // }
+       // System.out.println(Arrays.toString(myArray));
+
+    //4. Given array 1: [1,7,6,5,9] and array 2: [2,7,6,3,4]
+        //Write a program that will print out all pairs from arrays 1 and 2 that gives a sum of 13.
+       // int[] numA = {1,7,5,6,9};
+     //   int[] numB = {2,7,6,3,4};
+
+    //    for (int x : numA){
+       //     for (int y : numB){
+        //        if(x + y ==13){
+             //       System.out.println("(" + x + ", " + y + ")");
+             //   }
+          // 5. Prompt the user to input 10 values and store them into an array. Output the total number of odd and even values in the array.
+            //    int pr5 [] = new int[10];
+             //   int odds = 0;
+           //     int  evens = 0;
+           //     for (int i = 0; i<pr5.length; i++) {
+
+            //        System.out.println("Type in 10 values: " + (i + 1));
+             //       pr5[i] = reader.nextInt();
+               //     if (pr5[i]%2==0){
+                //        evens=+1;
+                //    } else {
+                 //       odds=+1;
+                 //   }
+           //     }
+             //   System.out.println("Even numbers: " + evens);
+              //  System.out.println("Odd numbers: " + odds);
+
+
+
+            }
+        }
+
+
+
+
